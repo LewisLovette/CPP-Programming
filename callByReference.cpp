7dd02c4dbd11feed660eb4b9d10b5e59dd8f9fe3
lovettel <lewislovette3@hotmail.co.uk>
2018-02-14 23:50:36 +0000

    Checking if update needed

--- callByReference.cpp
+++ callByReference.cpp
@@ -1,21 +1,13 @@
-#include <string>
+#include <iostream>
 
-class test1(){
-	int a, b, c;
-	String abc;
-	test1(int d, String s){
-		a = d;
-		abc = s;
-	}
-}
+void increment(int *v) {	//As we pass the pointer it means we are able to change the variable
+    (*v)++;					//even though it's not in the scope of it's functions
+}							//Sendin non-pointer would mutate a copy of it, as the original wouldn't be in it's scope.
 
-class test2(Object& o){
-
-};
-
-int main(){
-
-
-
-	return 0;
-};
+int main() {
+    int a;
+    scanf("%d", &a);
+    increment(&a);
+    printf("%d", a);
+    return 0;
+} 
